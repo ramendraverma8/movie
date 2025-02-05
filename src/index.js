b32a436571fd9e1cdfdd0bb33f5a5cb86b9ce0b3
Ramendra <ramendraverma8@gmail.com>
2025-02-05 16:35:26 +0530

    Add AWS RUM web client integration and update dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AwsRum } from 'aws-rum-web';
+try {
+  const config = {
+    sessionSampleRate: 1,
+    identityPoolId: "us-east-1:bbe03ce6-de54-40a5-b1dd-5b00be6448eb",
+    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+    telemetries: ["performance","errors","http"],
+    allowCookies: true,
+    enableXRay: true
+  };
+
+  const APPLICATION_ID = 'da1b6126-b81a-45d3-8613-276ad23d1e79';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'us-east-1';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
